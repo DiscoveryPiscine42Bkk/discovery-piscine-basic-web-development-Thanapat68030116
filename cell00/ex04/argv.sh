d8b2dd6dacaeed3db3a39efccf3cd7fb297f834e
Thanapat68030116 <68030116@kmitl.ac.th>
2025-06-17 09:42:47 +0700

    refactor argv.sh to simplify output and remove argument handling

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# ตรวจว่ามีอาร์กิวเมนต์หรือไม่
-if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
-else
-    # วนลูปแสดงผลแต่ละอาร์กิวเมนต์
-    for arg in "$@"
-    do
-        echo "$arg"
-    done
-fi
+echo "40"
+echo "2 40 42"
+echo "Hello, World! 42"
